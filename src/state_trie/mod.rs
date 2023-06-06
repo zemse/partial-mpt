823a4c7cf4031a3ae1cbad9e7fc22c03e73db60e
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-06-06 11:30:27 +0530

    Infer addresses programatically from block for tests

--- src/state_trie/mod.rs
+++ src/state_trie/mod.rs
@@ -99,7 +99,7 @@ mod tests {
     use super::{EIP1186ProofResponse, StateTrie, U256};
     use ethers::core::utils::hex;
     use ethers::providers::{Middleware, Provider};
-    use ethers::types::{BigEndianHash, BlockId, BlockNumber, StorageProof, H256};
+    use ethers::types::{Address, BigEndianHash, BlockId, BlockNumber, StorageProof, H256};
     use ethers::utils::keccak256;
 
     #[test]
@@ -211,10 +211,10 @@ mod tests {
         test_mainnet_block(
             1000024,
             vec![
-                ("0xD34DA389374CAAD1A048FBDC4569AAE33fD5a375", vec![]), // miner
-                ("0x2a65aca4d5fc5b5c859090a6c34d164135398226", vec![]), // tx1 - sender
-                ("0xf27b8f9e16d5b673c0a730f1994e1a588b221620", vec![]), // tx1 - dest
-                ("0x45c1392523399c1ce21ead4ecb808606c189fac2", vec![]), // tx2 - sender
+                // ("0xD34DA389374CAAD1A048FBDC4569AAE33fD5a375", vec![]), // miner
+                // ("0x2a65aca4d5fc5b5c859090a6c34d164135398226", vec![]), // tx1 - sender
+                // ("0xf27b8f9e16d5b673c0a730f1994e1a588b221620", vec![]), // tx1 - dest
+                // ("0x45c1392523399c1ce21ead4ecb808606c189fac2", vec![]), // tx2 - sender
                 (
                     "0xc7696b27830dd8aa4823a1cba8440c27c36adec4", // tx2 - dest
                     vec![
@@ -224,8 +224,8 @@ mod tests {
                         H256::from_uint(&U256::from(0xb)),
                     ],
                 ),
-                ("0x120a270bbc009644e35f0bb6ab13f95b8199c4ad", vec![]), // tx3 - sender
-                ("0x640d323222b99f3477339ff1639dcd66a93819fe", vec![]), // tx3 - dest
+                // ("0x120a270bbc009644e35f0bb6ab13f95b8199c4ad", vec![]), // tx3 - sender
+                // ("0x640d323222b99f3477339ff1639dcd66a93819fe", vec![]), // tx3 - dest
             ],
         )
         .await;
@@ -238,13 +238,13 @@ mod tests {
         test_mainnet_block(
             2000002,
             vec![
-                ("0xAdd823F3B2a13fA365f03d8D59ac3f017b15dB02", vec![]), // miner
-                ("0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8", vec![]), // tx1 - sender
-                ("0xCFc4EC1faA4f91F6E917e9e556320c841DA8588d", vec![]), // tx1 - dest
-                ("0xBa562F24005910e25dfaf2A42FeA4C0BF8A2D1c5", vec![]), // tx2 - dest
-                ("0xA5cebfe4aCc8d63B3257AC7f33677B987693072a", vec![]), // tx3 - dest
-                ("0x8d0e7c2C82970C9d1B7d598Be20F2bA0f13E6537", vec![]), // tx4 - dest
-                ("0xE68D2582D54Cd865C8FecD9733525d60BE68af67", vec![]), // tx5 - sender
+                // ("0xAdd823F3B2a13fA365f03d8D59ac3f017b15dB02", vec![]), // miner
+                // ("0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8", vec![]), // tx1 - sender
+                // ("0xCFc4EC1faA4f91F6E917e9e556320c841DA8588d", vec![]), // tx1 - dest
+                // ("0xBa562F24005910e25dfaf2A42FeA4C0BF8A2D1c5", vec![]), // tx2 - dest
+                // ("0xA5cebfe4aCc8d63B3257AC7f33677B987693072a", vec![]), // tx3 - dest
+                // ("0x8d0e7c2C82970C9d1B7d598Be20F2bA0f13E6537", vec![]), // tx4 - dest
+                // ("0xE68D2582D54Cd865C8FecD9733525d60BE68af67", vec![]), // tx5 - sender
                 (
                     "0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413", // tx5 - dest
                     vec![H256::from_str(
@@ -252,28 +252,28 @@ mod tests {
                     )
                     .unwrap()],
                 ),
-                ("0x5410C4c59A719EeF345869c204eC8Dcac2dfD718", vec![]), // tx6 - sender
-                ("0xa795B4DDBF17570f2c8f5e75a13EF94DB6757F32", vec![]), // tx6 - dest
-                ("0x4B8a25120682Dc5a5696D21C6D65f98442C7752F", vec![]), // tx7 - sender
+                // ("0x5410C4c59A719EeF345869c204eC8Dcac2dfD718", vec![]), // tx6 - sender
+                // ("0xa795B4DDBF17570f2c8f5e75a13EF94DB6757F32", vec![]), // tx6 - dest
+                // ("0x4B8a25120682Dc5a5696D21C6D65f98442C7752F", vec![]), // tx7 - sender
                 (
                     "0xb4bfEfC30A60B87380e377F8B96CC3b2E65A8F64", // tx7 - dest
                     vec![H256::from_uint(&U256::from(7))],
                 ),
+                // ("0x6bd0F7D1c3b3eB168b9CA96BD7872A41e2D4EEc5", vec![]), // tx8 - sender
+                // ("0xAA1A6e3e6EF20068f7F8d8C835d2D22fd5116444", vec![]), // tx8 - dest
+                // ("0x32Be343B94f860124dC4fEe278FDCBD38C102D88", vec![]), // tx9 - sender
+                // ("0x06c69F734A0240Cb31678412F04EB0CE8F39258b", vec![]), // tx9 - dest
+                // ("0x147184Ef469cE9Bba3d08aF16F0b6d31CAC35ac8", vec![]), // tx10 - sender
+                // ("0x695055f1EA55c36EC7E3Bd43D1736511ac8daB61", vec![]), // tx10 - dest
                 ("0xe77f55ea0c862c78dd02dcf8828e5046c98ef97c", vec![]), // tx7 - internal
                 ("0x5ec0f3f946525abc4cccb433e6fc54df6d55e192", vec![]), // tx7 - internal
-                ("0x6bd0F7D1c3b3eB168b9CA96BD7872A41e2D4EEc5", vec![]), // tx8 - sender
-                ("0xAA1A6e3e6EF20068f7F8d8C835d2D22fd5116444", vec![]), // tx8 - dest
                 ("0x4af482be804f3c925f0b405d871fa53acaa91fbb", vec![]), // tx8 - internal
-                ("0x32Be343B94f860124dC4fEe278FDCBD38C102D88", vec![]), // tx9 - sender
-                ("0x06c69F734A0240Cb31678412F04EB0CE8F39258b", vec![]), // tx9 - dest
-                ("0x147184Ef469cE9Bba3d08aF16F0b6d31CAC35ac8", vec![]), // tx10 - sender
-                ("0x695055f1EA55c36EC7E3Bd43D1736511ac8daB61", vec![]), // tx10 - dest
             ],
         )
         .await;
     }
 
-    pub async fn test_mainnet_block(block_number: u64, accounts_touched: Vec<(&str, Vec<H256>)>) {
+    pub async fn test_mainnet_block(block_number: u64, account_slots: Vec<(&str, Vec<H256>)>) {
         let rpc_url = env::var("RPC").expect("pass RPC env var");
         let provider = Provider::try_from(rpc_url).unwrap();
 
@@ -285,10 +285,33 @@ mod tests {
             .await
             .unwrap()
             .unwrap();
+        let current_block = provider
+            .get_block_with_txs(current_block_number.unwrap())
+            .await
+            .unwrap()
+            .unwrap();
+
         let mut state_trie = StateTrie::from_root(prev_block.state_root);
 
-        // download EIP-1186 state proofs.
-        for (address, slots) in accounts_touched.clone() {
+        // pick addresses which might have their state changed from block data
+        let mut account_slots_parsed: Vec<(Address, Vec<H256>)> = account_slots
+            .clone()
+            .iter()
+            .map(|(addr, slots)| (addr.parse::<Address>().unwrap(), slots.clone()))
+            .collect();
+        account_slots_parsed.push((current_block.author.unwrap(), vec![]));
+        for tx in current_block.transactions {
+            account_slots_parsed.push((tx.from, vec![]));
+            if tx.to.is_some() {
+                account_slots_parsed.push((tx.to.unwrap(), vec![]));
+            } else {
+                let contract_address = ethers::utils::get_contract_address(tx.from, tx.nonce);
+                account_slots_parsed.push((contract_address, vec![]));
+            }
+        }
+
+        // download EIP-1186 state proofs for addresses and slots
+        for (address, slots) in account_slots_parsed.clone() {
             let proof = provider
                 .get_proof(address, slots, prev_block_number)
                 .await
@@ -298,9 +321,7 @@ mod tests {
         }
 
         // update state on our trie.
-        for (address_str, slots) in accounts_touched {
-            let address = address_str.parse().unwrap();
-
+        for (address, slots) in account_slots_parsed {
             let new_balance = provider
                 .get_balance(address, current_block_number)
                 .await
@@ -342,14 +363,7 @@ mod tests {
             }
         }
 
-        let current_block = provider
-            .get_block(current_block_number.unwrap())
-            .await
-            .unwrap()
-            .unwrap();
-
         let calculated_root = state_trie.root().unwrap();
-
         assert_eq!(calculated_root, current_block.state_root);
     }
 }
